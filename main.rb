d7c81bc7c8f2a196c6a3615a01e7a688b766f775
Sophie Smithburg <sophie@heartpunk.com>
2021-10-08 11:10:17 -0400

    some simplifications to main.rb

--- main.rb
+++ main.rb
@@ -6,6 +6,18 @@ require "pp"
 files = Dir.glob("**/*.rb")
 bar = ProgressBar.new(files.length)
 
+def ast_node? maybe_node
+  has_children(maybe_node) && locatable?(:location)
+end
+
+def has_children? a
+  a.respond_to? :location
+end
+
+def locatable? a
+  a.respond_to? :location
+end
+
 files.map do |file|
   bar.increment!
 
@@ -15,8 +27,8 @@ files.map do |file|
     nodes = [ast]
     while !nodes.empty? do
       node = nodes[-1]
-      if visited_nodes.include?(node) || !node.respond_to?(:children)
-        pp node.type, node.location.to_hash.to_a if node.respond_to?(:location)
+      if visited_nodes.include?(node) || !has_children?(node)
+        pp node.type, node.location.to_hash.to_a if locatable?(node)
         nodes.pop
       else
         nodes.concat(node.children.to_a)
